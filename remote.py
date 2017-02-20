1f25599ea2436c555267a1e8825bd081f182ded9
xintong <exinton@gmail.com>
2017-02-20 15:03:06 -0600

    ok

--- remote.py
+++ remote.py
@@ -1,5 +1,11 @@
 import os
-os.system('sudo python3 irtoy.py play mute.bin')
+import sys
 
+def remote(cmd):
+	os.system('sudo python3 irtoy.py play '+cmd+'.bin')
+def main():
+	cmd=sys.argv[1]
+	remote(cmd)
 
-
+if __name__=="__main__":
+	main()
