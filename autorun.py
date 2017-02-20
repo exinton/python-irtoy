7d0699ad379881e7b7e215e081f4383e3b408722
Xin Tong <xtong@roku.com>
2017-02-20 14:58:55 -0600

    add auto

---
+++ autorun.py
@@ -0,0 +1,31 @@
+import remote.py
+
+
+
+def changePictureSize():
+    #check the picture size
+    remote(info)
+    remote(down)
+    remote(right)
+    time.sleep(2)
+    remote(right)
+    time.sleep(2)
+    remote(right)
+    time.sleep(2)
+    remote(right)
+    time.sleep(2)
+def checkResolution():
+    #check the picture resolution
+    remote(info)
+    time.sleep(2)
+    remote(info)
+
+                      
+def main():
+    changePictureSize()
+    checkResolution()
+
+
+
+if __name__ == "__main__":
+    main()
