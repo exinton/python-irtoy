9bebefc8ea3d5c4710d4d63a584dd4edf381889d
xintong <exinton@gmail.com>
2017-02-20 14:34:31 -0600

    add one button py

---
+++ remote.py
@@ -0,0 +1,5 @@
+import os
+os.system('sudo python3 irtoy.py play mute.bin')
+
+
+
--- test.sh
+++ test.sh
@@ -4,17 +4,17 @@
 while [ 1 -eq 1 ]
 do
 	sudo python3 irtoy.py play left.bin
-	sleep 3
+	//sleep 3
 	sudo python3 irtoy.py play right.bin
-	sleep 3
-	sudo python3 irtoy.py play left.bin
-	sleep 3
+	//sleep 3
+	//sudo python3 irtoy.py play left.bin
+	//sleep 3
 	sudo python3 irtoy.py play volumeup.bin
-	sleep 3
+	//sleep 3
 	sudo python3 irtoy.py play mute.bin
-	sleep 3
+	//sleep 3
 	sudo python3 irtoy.py play volumedown.bin
-	sleep 3
+	//sleep 3
 done
 
 
